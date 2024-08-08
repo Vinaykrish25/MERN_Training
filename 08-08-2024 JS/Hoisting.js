551955ac00f9af5f75ba2d2c5a333ada47591706
Vinaykrish25 <vinaykrish2002@gmail.com>
2024-08-08 17:15:34 +0530

    08-08-2024 Practice files during class

---
+++ 08-08-2024 JS/Hoisting.js
@@ -0,0 +1,10 @@
+click()
+function click(){
+    console.log("Hi")
+}
+
+
+// click()
+// const click = function(){
+//     console.log("Hi")
+// }
---
+++ 08-08-2024 JS/mouseover_mouseout.js
@@ -0,0 +1,16 @@
+const add = function(){
+    const data = document.getElementById("box")
+    data.style.backgroundColor = "purple"
+    data.style.color = "white"
+    data.innerHTML = "purple"
+    data.style.fontSize = "50px"
+   
+}
+const add1 = function(){
+    const data1 = document.getElementById("box")
+    data1.style.backgroundColor = "blue"
+    data1.style.color = "white"
+    data1.innerHTML = "blue"
+    data1.style.fontSize = "50px"
+ 
+}
